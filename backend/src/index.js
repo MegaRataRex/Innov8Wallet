d3a9f892928775fdd79e03fa12450bfd3e43c756
santy <santybel.1@hotmail.com>
2025-02-09 19:33:08 -0700

    limpieza

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API de la app bancaria funcionando!");
+});
+
+app.listen(3000, () => console.log("Backend corriendo en http://localhost:3000"));
