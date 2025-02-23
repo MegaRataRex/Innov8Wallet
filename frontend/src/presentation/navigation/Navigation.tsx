27da5e371d1a66bc58c01520b9c85baa5e1e0030
santy <santybel.1@hotmail.com>
2025-02-23 13:02:39 -0700

    navigation

---
+++ frontend/src/presentation/navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/HomeScreen';
+
+export type RootStackParams={
+    Home: undefined
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+    </Stack.Navigator>
+  );
+};
