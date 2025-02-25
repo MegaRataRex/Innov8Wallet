e2386b6887222a3e0cc5efea3ad2b36a57f66434
ELopezSingh <3miliano.l0pez274@gmail.com>
2025-02-24 21:13:12 -0700

    frontend😭

--- frontend/src/presentation/navigation/Navigation.tsx
+++ frontend/src/presentation/navigation/Navigation.tsx
@@ -1,16 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
+import  CardsPage  from '../screens/CardsPage';
 
 export type RootStackParams={
     Home: undefined
+    Cards: undefined
 }
 
 const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Cards" component={CardsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
